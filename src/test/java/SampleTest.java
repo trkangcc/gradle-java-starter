d2c64a80271701625079691b1841dd9971020902
강태룡 <trkangcc@gmail.com>
2020-05-15 22:23:29 +0900

    최초 파일 추가.

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    public void test_001(){
+        Assertions.assertEquals(1, 1);
+    }
+}
